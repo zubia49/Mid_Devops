a18cef8690c963bf5404d2893fcbfbd7e9dc81bf
zubia49 <zubiafiaz498@gmail.com>
2025-11-17 12:38:11 +0500

    Changed in jenkins pipeline to check againn

--- src/pages/home.js
+++ src/pages/home.js
@@ -4,7 +4,7 @@ function Home() {
   return (
     <div style={{textAlign:"center", padding:"50px"}}>
       <h1>Home Page!!!!!!!</h1>
-      <p>Welcome! You are now logged in to the jenkins .</p>
+      <p>Welcome! You are now logged in to home "the jenkins" .</p>
     </div>
   );
 }
