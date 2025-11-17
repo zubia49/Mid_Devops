a5bbb52be89911f68608f62b46409746cb6acb76
zubia49 <zubiafiaz498@gmail.com>
2025-11-17 12:22:41 +0500

    Changed in jenkins pipeline to check

--- src/pages/home.js
+++ src/pages/home.js
@@ -4,7 +4,7 @@ function Home() {
   return (
     <div style={{textAlign:"center", padding:"50px"}}>
       <h1>Home Page</h1>
-      <p>Welcome! You are logged in.</p>
+      <p>Welcome! You are logged in to the jenkins .</p>
     </div>
   );
 }
