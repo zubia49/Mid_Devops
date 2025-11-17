6387ccd6a76dcdd19335844d9fcaeb93b54abd84
zubia49 <zubiafiaz498@gmail.com>
2025-11-17 12:26:59 +0500

    Changed in jenkins pipeline to check again

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,7 +3,7 @@ import React from "react";
 function Home() {
   return (
     <div style={{textAlign:"center", padding:"50px"}}>
-      <h1>Home Page</h1>
+      <h1>Home Page!!!</h1>
       <p>Welcome! You are logged in to the jenkins .</p>
     </div>
   );
